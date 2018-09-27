9c5224a3a064e7f11f0da690fe57fc8d12142071
Jttearpock <43620244+Jttearpock@users.noreply.github.com>
2018-09-27 15:43:02 -0500

    Game class
    Added game class and arrays

---
+++ SudokuGame/SudokuGame/Game Classes/ActiveGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuGame
+{
+    public class ActiveGame
+    {
+        public int[,] ArrPuzzleSolution;
+        public int[,] ArrPuzzleBase;
+        public int[,] ArrPuzzleCurrent;
+    }
+}
